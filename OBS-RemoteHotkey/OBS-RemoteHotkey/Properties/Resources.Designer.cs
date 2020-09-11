47060e2226935522486422bbd1a40d0b42ca22fe
Markus <48181660+BackInBash@users.noreply.github.com>
2020-09-11 20:20:28 +0200

    Changed Icons

--- OBS-RemoteHotkey/OBS-RemoteHotkey/Properties/Resources.Designer.cs
+++ OBS-RemoteHotkey/OBS-RemoteHotkey/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace OBS_RemoteHotkey.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon obs_icon {
+            get {
+                object obj = ResourceManager.GetObject("obs_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
